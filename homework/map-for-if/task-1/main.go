cfb4554b305729accbeb1e81956882cbfa480356
lifeho5e <lifehouseburning@gmail.com>
2022-06-21 08:18:05 +0300

    some changes again

--- homework/map-for-if/task-1/main.go
+++ homework/map-for-if/task-1/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 )
 
 func main() {
 	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
-	// var resault []int
+	var resault []int
+	var temp []int
 	sort.Ints(arr)
 
-	// for i:=0; i < len(arr); i++ {
-	// 	if arr[i] ==
-	// }
-
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] == arr[i+1] {
+			resault = append(arr[i:])
+		}
+	}
+	fmt.Println(resault, temp)
 }
