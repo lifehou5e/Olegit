b9c32ac32040d754fb064c9f88262ff2cdb9d5a5
lifeho5e <lifehouseburning@gmail.com>
2022-06-21 10:40:03 +0300

    task complited i guess

--- homework/map-for-if/task-1/main.go
+++ homework/map-for-if/task-1/main.go
@@ -10,11 +10,18 @@ func main() {
 	var resault []int
 	var temp []int
 	sort.Ints(arr)
-
+	fmt.Println(arr)
 	for i := 0; i < len(arr)-1; i++ {
-		if arr[i] == arr[i+1] {
-			resault = append(arr[i:])
+		if arr[i] != arr[i+1] {
+			resault = append(resault, arr[i])
 		}
 	}
-	fmt.Println(resault, temp)
+	var lengthArr = len(arr)
+	for i := 1; i < lengthArr-1; i++ {
+		if arr[lengthArr-i] != arr[lengthArr-i-1] {
+			temp = append(temp, arr[lengthArr-i])
+		}
+	}
+	resault = append(resault, temp[0])
+	fmt.Println(resault)
 }
