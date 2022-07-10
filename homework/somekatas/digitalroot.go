46d2581d5a5852bc1984484d7f8abf5eac9aadf1
lifeho5e <lifehouseburning@gmail.com>
2022-07-10 18:30:04 +0300

    digital root task complited

--- homework/somekatas/digitalroot.go
+++ homework/somekatas/digitalroot.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(DigitalRoot(0))
+}
+
+func DigitalRoot(n int) int {
+	nStr := strconv.Itoa(n)
+	nList := strings.Split(nStr, "")
+	var resultSum int
+	for _, v := range nList {
+		a, _ := strconv.Atoi(v)
+		resultSum += a
+	}
+	if resultSum > 10 {
+		return 0 + DigitalRoot(resultSum)
+	} else {
+		return resultSum
+	}
 }
