527d3fbc879d4b00cb73300f8c7ced396816c55d
lifehou5e <lifehouseburn@gmail.com>
2022-08-08 16:42:39 +0300

    adding some error handling

--- convertProject/main.go
+++ convertProject/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tealeg/xlsx"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +16,7 @@ const (
 	st3        = "ст3"
 	oc         = "оц"
 	steel09g2s = "09Г2С"
+	exit       = "выход"
 )
 
 func main() {
@@ -33,13 +35,31 @@ func main() {
 	var outSlice []string
 	rangeLow := 0
 	rangeHigh := 0
-	fmt.Println("Enter range of rows to work with")
-	fmt.Scan(&rangeLow, &rangeHigh)
+	fmt.Println("Введите диапазон для обработки:")
+	_, err = fmt.Scan(&rangeLow, &rangeHigh)
+	if err != nil {
+		fmt.Println("Проверьте типы входных параметров")
+		return
+	}
+	if rangeLow > rangeHigh {
+		fmt.Println(errors.New("ошибка: нижняя граница не может быть больше верхней"))
+		return
+	}
 	var steelParam, thicknessParam string
-	fmt.Println("Enter what kind of steel do you want to work with: (ст3, оц, 09г2с)")
-	fmt.Scan(&steelParam)
-	fmt.Println("Enter sheet thickness: (1,1.5,2,2.5,3,4)")
+	steelParam, err = inputSteel()
+	if steelParam == exit {
+		return
+	}
+	fmt.Println("Введите толщину листа: (1,2,3,4)")
 	fmt.Scan(&thicknessParam)
+	converted, err := strconv.Atoi(thicknessParam)
+	if err != nil {
+		return
+	}
+	if converted > 4 || converted < 1 {
+		fmt.Println(errors.New("ошибка: заданная толщина не обрабатывается, либо введена неверно"))
+		return
+	}
 	if steelParam == st3 {
 		for i := rangeLow; i < rangeHigh; i++ {
 			if strings.Contains(sh.Cell(i, columnB).String(), st3) {
@@ -59,7 +79,7 @@ func main() {
 			}
 		}
 	} else {
-		err := errors.New("wrong sheet material input")
+		err := errors.New("неправильно введён материал")
 		fmt.Println(err)
 	}
 
@@ -91,7 +111,10 @@ func main() {
 		}
 	}
 	sort.Strings(outSlice)
-	fmt.Printf("%v", outSlice)
+	if len(outSlice) == 0 {
+		errEmptyOut := errors.New("в данном диапазоне металла задонного типа не обнаружено")
+		fmt.Println(errEmptyOut)
+	}
 	file, err := os.Create("test.txt")
 	if err != nil {
 		panic(err)
@@ -99,5 +122,27 @@ func main() {
 	defer file.Close()
 	for _, v := range outSlice {
 		file.WriteString(v)
+	}
+}
+
+//Функция inputSteel сканирует ввод стали и если он не соответствуем трём вариантам, выводит ошибку и вызывает
+//функцию по новой пока не будетт введён верный
+func inputSteel() (string, error) {
+	var steelParam string
+	fmt.Println("Введите сталь, с которой хотите работать: (ст3, оц, 09г2с)")
+	fmt.Println("Введите \"выход\" для выхода из программы")
+	fmt.Scan(&steelParam)
+	switch steelParam {
+	case st3:
+		return st3, nil
+	case oc:
+		return oc, nil
+	case steel09g2s:
+		return steel09g2s, nil
+	case exit:
+		return exit, nil
+	default:
+		fmt.Println(errors.New("ошибка: неверно введён материал"))
+		return inputSteel()
 	}
 }
